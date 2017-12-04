cb83518f66ee742abb2417d0fbdedab600a10add
Pongpong <pong7658@gmail.com>
2017-12-04 13:48:12 +0700

    tsst

--- src/Back1.java
+++ src/Back1.java
@@ -1,8 +1,11 @@
 
 public class Back1 {
 	public static void main() {
-				
-		
+		System.out.println("Hello java");	
+		int[] array = {1,3,4,6};
+		for (int i:array) {
+			System.out.println(i);
+		}
 		
 	}
 }
